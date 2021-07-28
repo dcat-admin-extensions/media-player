85f25526ba8ab349ba618d71df85dae51df06771
abovesky <abovesky@qq.com>
2021-07-28 20:08:44 +0800

    代码优化

--- src/MediaPlayerServiceProvider.php
+++ src/MediaPlayerServiceProvider.php
@@ -6,25 +6,27 @@ use Abovesky\DcatAdmin\MediaPlayer\Grid\AudioDisplayer;
 use Abovesky\DcatAdmin\MediaPlayer\Grid\VideoDisplayer;
 use Abovesky\DcatAdmin\MediaPlayer\Show\AudioField;
 use Abovesky\DcatAdmin\MediaPlayer\Show\VideoField;
+use Dcat\Admin\Admin;
 use Dcat\Admin\Extend\ServiceProvider;
 use Dcat\Admin\Grid\Column;
 use Dcat\Admin\Show\Field;
 
 class MediaPlayerServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
     public function init()
     {
         parent::init();
 
-        Column::extend('video', VideoDisplayer::class);
-        Column::extend('audio', AudioDisplayer::class);
-        Field::extend('video', VideoField::class);
-        Field::extend('audio', AudioField::class);
+        if ($views = $this->getViewPath()) {
+            $this->loadViewsFrom($views, 'dcat-media-player');
+        }
+
+        Admin::booting(function () {
+            Column::extend('video', VideoDisplayer::class);
+            Column::extend('audio', AudioDisplayer::class);
+            Field::extend('video', VideoField::class);
+            Field::extend('audio', AudioField::class);
+        });
     }
 
     public function settingForm()
